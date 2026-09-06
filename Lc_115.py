b50b611d3195abbdc194e941b6f5aea8372f3a17
Vineet12-ranjan <kumarrajeevranjan108@gmail.com>
2026-09-07 00:04:37 +0530

    Add numDistinct method to Solution class
    Implement dynamic programming solution for counting distinct subsequences.

---
+++ Lc_115.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numDistinct(self, s, t):
+        m = len(t)
+        dp = [0] * (m + 1)
+        dp[0] = 1
+
+        for c in s:
+            for j in range(m, 0, -1):
+                if c == t[j - 1]:
+                    dp[j] += dp[j - 1]
+
+        return dp[m]
